66b29f21491f008fc97764622e8154112346b693
Hu <Xiaolin.Hu@cchmc.org>
2020-01-14 17:24:42 -0500

    Add comments

--- Example1.R
+++ Example1.R
@@ -1,11 +1,11 @@
 file <- "TCGA_breast_cancer_LumA_vs_Basal_PAM50.tsv"
 first10 <- c('NAT1','BIRC5','BAG1','BCL2','BLVRA','CCNB1','CCNE1','CDC6','CDC20','CDH3')
 nfold <- 3
-
+# reads sample labels
 header <- scan(file, nlines = 1, sep="\t", what = character())
 data <- read.table(file, skip = 2, header = FALSE, sep = "\t", quote = "", check.names=FALSE)
 names(data) <- header
-
+# reads sample ER +/-
 header2 <- scan(file, skip = 1, nlines = 1, sep="\t", what = character())
 
 LumA <- data[data$sample_id %in% first10,header2=='Luminal A']
